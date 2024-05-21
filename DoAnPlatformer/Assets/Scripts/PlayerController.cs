2874637c88a459e11056ff123e923c9af3d5399e
Viet181099 <64130233+Viet181099@users.noreply.github.com>
2024-05-21 21:01:26 +0700

    Move And Flip Character
    Clean Code

--- DoAnPlatformer/Assets/Scripts/PlayerController.cs
+++ DoAnPlatformer/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,40 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
     Rigidbody2D myRB;
+    SpriteRenderer sRender;
+    bool isFacingRight;
 
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
-    }
-
     
+    }
+ 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        myRB.velocity = new Vector2 (horizontal * speed, 0);
+        PlayerMovement();
     }
+    
 
-
+    public void PlayerMovement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        myRB.velocity = new Vector2(horizontal * speed , 0);
+        if (horizontal > 0 && isFacingRight)
+        {
+            FlipX();
+        }
+        else if(horizontal < 0 && !isFacingRight)
+        {
+            FlipX();
+        }
+    }  
+    protected void FlipX()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+        
 }
