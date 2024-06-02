9f69aff640099c602012fd318182db49babd7b77
VuongTranKameru <0918051090t@gmail.com>
2024-06-02 21:47:59 +0700

    Adding Pause Menu, Loader

---
+++ DoAnPlatformer/Assets/Scripts/SceneControllers/UISystemManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISystemManager : MonoBehaviour
+{
+    internal static UISystemManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
