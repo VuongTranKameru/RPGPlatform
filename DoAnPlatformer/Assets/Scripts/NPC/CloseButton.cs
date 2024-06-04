2001d633f97961fde7c102305848e25442fcdf40
Lam <165276530+thl08121999@users.noreply.github.com>
2024-06-04 19:26:44 +0700

    addCloseButton

---
+++ DoAnPlatformer/Assets/Scripts/NPC/CloseButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseButton : MonoBehaviour
+{
+    public GameObject closeButton;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void CloseButtonl()
+    {
+
+            closeButton.SetActive(false);
+        
+    }
+
+}
